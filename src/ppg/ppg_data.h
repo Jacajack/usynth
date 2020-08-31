9f193f01cdff0350483506706d3d4e9ea279d82a
Jacek Wieczorek <mrjjot@gmail.com>
2020-08-31 22:14:02 +0200

    Add wave sweep demo

---
+++ src/ppg/ppg_data.h
@@ -0,0 +1,10 @@
+#ifndef PPG_DATA_AVR_H
+#define PPG_DATA_AVR_H
+
+#include <inttypes.h>
+#include <avr/pgmspace.h>
+
+extern const uint8_t ppg_wavetable[] PROGMEM;
+extern const uint8_t ppg_waveforms[] PROGMEM;
+
+#endif
