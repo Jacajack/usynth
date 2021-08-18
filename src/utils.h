5c63190cd35a74f0413700acdf5b540c59ed5d33
Jacek Wieczorek <mrjjot@gmail.com>
2021-08-18 20:19:30 +0200

    Slightly optimize `safe_add_*()` functions

--- src/utils.h
+++ src/utils.h
@@ -11,31 +11,31 @@
 //! Safe int8_t add (no overflow and underflow)
 static inline int8_t safe_add_8(int8_t a, int8_t b)
 {
-	if (a > 0 && b > INT8_MAX - a)
-		return INT8_MAX;
-	else if (a < 0 && b < INT8_MIN - a)
-		return INT8_MIN;
-	return a + b;
+    int8_t r = (uint8_t)a + (uint8_t)b;
+    uint8_t asgn = (a & (1 << 7)) != 0;
+    if (!asgn && r < b) return INT8_MAX; 
+    else if (asgn && r > b) return INT8_MIN;
+    else return r;
 }
 
 //! Safe int16_t add (no overflow and underflow)
 static inline int16_t safe_add_16(int16_t a, int16_t b)
 {
-	if (a > 0 && b > INT16_MAX - a)
-		return INT16_MAX;
-	else if (a < 0 && b < INT16_MIN - a)
-		return INT16_MIN;
-	return a + b;
+    int16_t r = (uint16_t)a + (uint16_t)b;
+    uint8_t asgn = (a & (1 << 15)) != 0;
+    if (!asgn && r < b) return INT16_MAX; 
+    else if (asgn && r > b) return INT16_MIN;
+    else return r;
 }
 
 //! Safe int32_t add (no overflow and underflow)
 static inline int32_t safe_add_32(int32_t a, int32_t b)
 {
-	if (a > 0 && b > INT32_MAX - a)
-		return INT32_MAX;
-	else if (a < 0 && b < INT32_MIN - a)
-		return INT32_MIN;
-	return a + b;
+    int32_t r = (uint32_t)a + (uint32_t)b;
+    uint8_t asgn = (a & (1ul << 31)) != 0;
+    if (!asgn && r < b) return INT32_MAX; 
+    else if (asgn && r > b) return INT32_MIN;
+    else return r;
 }
 
 //! Read uint16_t from delta-compressed uint8_t array in PROGMEM
